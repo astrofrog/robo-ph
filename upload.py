50ab0d3b5a5351c7cd87ab8a079a73f744c03b33
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-11-10 22:43:43 +0100

    Added missing RSS file and updated upload script

--- upload.py
+++ upload.py
@@ -1,10 +1,9 @@
+import os
 import shutil
 import subprocess
 
 DROPBOX_DIR = os.path.expanduser("~/Dropbox/Public/robo-ph/")
 
-date = now.strftime("%Y-%m-%d")
-
-shutil.copy(date + '.m4a', os.path.join(DROPBOX_DIR, date + '.m4a'))
+subprocess.call('rsync -a final/ ~/Dropbox/Public/robo-ph/', shell=True)
 shutil.copy('rss/combined.rss', os.path.join(DROPBOX_DIR, 'roboph.rss'))
-subprocess.call('git add rss/combined.rss ; git commit rss/combined.rss', shell=True)
+subprocess.call('git add rss/*.rss ; git commit -m "Automated RSS feed commit"', shell=True)
