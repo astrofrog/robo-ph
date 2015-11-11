6eaaec80d17096df8ae8d68006bd58dd029b481d
AstroBot <tom@chi-squared.org>
2015-11-11 12:50:45 +0100

    Updated upload script

--- upload.py
+++ upload.py
@@ -1,9 +1,13 @@
 import os
+import time
 import shutil
 import subprocess
 
 DROPBOX_DIR = os.path.expanduser("~/Dropbox/Public/robo-ph/")
 
-subprocess.call('rsync -a final/ ~/Dropbox/Public/robo-ph/', shell=True)
+subprocess.call('rsync -a final/ {0}'.format(DROPBOX_DIR), shell=True)
 shutil.copy('rss/combined.rss', os.path.join(DROPBOX_DIR, 'roboph.rss'))
-subprocess.call('git add rss/*.rss ; git commit -m "Automated RSS feed commit"', shell=True)
+
+time.sleep(60)
+
+subprocess.call('git add rss/*.rss ; git commit -m "Automated RSS feed commit" ; git push origin master', shell=True)
